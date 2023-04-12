ad703f27fe4dadd77307a200bd1bc0d3fdaf7962
Blake Jacobs <blake.jacobs07@gmail.com>
2023-04-11 22:19:37 -0500

    Create files and declare basic CSS rules

---
+++ script.js
@@ -0,0 +1,10 @@
+const tiles = document.querySelector('.tiles');
+let gridSize = 16; 
+
+// create a variable for the grid size
+// create custom properties to define number of columns and rows in CSS 
+// define those custom properties as equal to gridSize variable
+// create function to create and append a number of tiles equal to gridSize squared to the parent container
+// add hover effects (only while mouse is held down) that change background color on mouse entry
+// create button that generates a prompt to ask the user to define gridSize
+// set gridSize to value identified by user
