72107f51107d60771ba9c3ce26b829167b6be886
Blake Jacobs <blake.jacobs07@gmail.com>
2023-04-12 21:14:45 -0500

    Create button and logic for grid creation

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 const tiles = document.querySelector('.tiles');
-let gridSize = 16; 
+const gridSizer = document.querySelector(".grid-size-select");
+const tile = document.querySelector ("tile");
+let gridSize; 
+
+function createTile () {
+    const tile = document.createElement("div"); 
+
+    tile.classList.add("tile"); 
+
+    return tile;
+}; 
+
+function createTiles (quantity) {
+    Array.from(Array(quantity)).map((tile) => {
+        tiles.appendChild(createTile());
+    })
+};
+
+function createGrid (gridSize) {
+    createTiles(gridSize*gridSize);
+    tiles.style.setProperty("--gridSize", gridSize)
+}
+
+
+function getGridSize () {
+    tiles.innerHTML = '';
+    gridSize = prompt('Enter a number between 1 and 100');
+    createGrid(gridSize);
+}
+
+gridSizer.addEventListener('click', getGridSize);
+
+; 
+
+// pseudocode
 
 // create a variable for the grid size
 // create custom properties to define number of columns and rows in CSS 
